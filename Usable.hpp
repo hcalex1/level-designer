8d47d47919ce3a0a57c0b644311661b4aeb202e0
Alex HC <hcalex1.accounts@hotmail.com>
2021-12-07 11:06:03 -0500

    added file header

---
+++ Usable.hpp
@@ -0,0 +1,16 @@
+/**
+* This file consists of the Usable abstract class  all classes that can 
+* be used by the player will be derrived from this class.
+* \file   main.cpp
+* \author Alex Hoang-Cao and Emile Watier
+* \date   13 December 2021
+* Created 13 December 2021
+*/
+
+#pragma once
+
+class Usable
+{
+public:
+    virtual void use();
+};
