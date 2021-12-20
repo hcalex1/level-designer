af92cf1dfc1c8f8485599f8b94b2b51527ebbfe5
Alex HC <hcalex1.accounts@hotmail.com>
2021-12-20 10:46:47 -0500

    completed declarations?

--- Object.hpp
+++ Object.hpp
@@ -11,12 +11,25 @@
 #include "Lookable.hpp"
 #include "Interactible.hpp"
 
-#include "iostream"
+#include <iostream>
+#include <string>
+#include <functional>
 
 class Object : public Lookable, public Interactible {
 public:
-    Object(const std::string&);
+    Object(const std::string&, const std::string&);
+    
+    const std::string& getName() const;
+    const std::string& getDescription() const ;
+
+    void setInteract(const std::function<void(Navigator&, Object&)> interactFunction, 
+                    std::string interactText = "Nothing to do");
 
     virtual void interact(Navigator&, std::ostream&) const override;
     virtual void show(std::ostream&) const override;
+
+private:
+    std::string name_;
+    std::string description;
+    std::string interactText_;
 };
